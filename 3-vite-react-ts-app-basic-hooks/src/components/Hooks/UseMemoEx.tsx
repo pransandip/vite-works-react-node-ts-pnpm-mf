8c3a84dd8dafb96f2db53372dd50acb67c1a04be
pransandip <[pransandip@gmail.com]>
2023-04-08 18:29:21 +0530

    feat: pnpm lock file & useMemoEx added

--- 3-vite-react-ts-app-basic-hooks/src/components/Hooks/UseMemoEx.tsx
+++ 3-vite-react-ts-app-basic-hooks/src/components/Hooks/UseMemoEx.tsx
@@ -1,8 +1,14 @@
-import { Fragment, memo, useState, useMemo } from "react";
+import { Fragment, memo, useState, useMemo, useCallback } from "react";
 
 type CProps = "red" | "blue";
 
-function Swatch({ params }: { params: { color: string } }) {
+function Swatch({
+  params,
+  onClick,
+}: {
+  params: { color: string };
+  onClick: () => void;
+}) {
   console.log(`Swatch rendered ${params.color}`);
 
   return (
@@ -14,6 +20,7 @@ function Swatch({ params }: { params: { color: string } }) {
         height: 75,
         backgroundColor: params.color,
       }}
+      onClick={onClick}
     ></div>
   );
 }
@@ -22,18 +29,22 @@ function Swatch({ params }: { params: { color: string } }) {
 const MemoedSwatch = memo(Swatch);
 
 const UseMemoEx = () => {
+  // Defined State
   const [appRenderIndex, setAppRenderIndex] = useState<number>(0);
   const [color, setColor] = useState<CProps>("red");
 
   console.log(`App rendered ${appRenderIndex}`);
 
+  // every time is returns same value until dependency array changes
   const params = useMemo(() => ({ color }), [color]);
+  // it returns exact same func by reference not func implementation
+  const onClick = useCallback(() => {}, []);
 
   return (
     <Fragment>
       <div>
         <h3>MemoEx</h3>
-        <MemoedSwatch params={params} />
+        <MemoedSwatch params={params} onClick={onClick} />
       </div>
       <div>
         <button onClick={() => setAppRenderIndex(appRenderIndex + 1)}>
